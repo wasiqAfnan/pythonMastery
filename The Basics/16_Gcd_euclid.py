41cf4360be0403b23424e1690227a23feacae89e
WasiqAfnan <107802276+wasiqAfnan@users.noreply.github.com>
2025-01-05 02:34:48 +0530

    Added recursive gcd algorithm

--- The Basics/16_Gcd_euclid.py
+++ The Basics/16_Gcd_euclid.py
@@ -5,6 +5,10 @@ def euclid_gcd(a,b):
     return b
     
 # Recursive approach
-
+def rec_gcd(a,b):
+    if a%b == 0:
+        return b
+    return rec_gcd(b, a%b)
         
 print(euclid_gcd(10,55)) # output: 5
+print(rec_gcd(10,55)) # output: 5
